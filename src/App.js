a3a546196959057bf1e7457fa39c62084c47a007
BBuehler24 <bbuehler47@yahoo.com>
2023-03-30 21:26:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
+
+// In above, we have to use "className" vs. just "class" because class is a reserved keyword in JS. The "className" is converted to just "class".
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
+//Click Events
+//UseState
+//Outputting Lists
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+
+    // want to find a way to cycle through the above object data & post dynamically to HTML/JSX
+    // Note: every <div> created from .map must include a key attribute
+
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs!"/> 
+        </div>
+);
+    }
+
+// const Home = () => {
+//     // let name = 'Brian';
+//     const [name, setName] = useState('Brian'); //defining name with original value of Brian. setName will be the reactive/changeable variable.
+//     const [age, setAge] = useState(25);
+
+//     const handleClick = () => {
+//         setName('luigi'); // changes the name value to luigi upon click
+//         setAge(30);
+//     }
+
+    // const handleClick = (e) => { //the handleCLick is a common naming function for react
+    //     console.log('hello, ninjas', e); // adding the 'e' here & as argument, allows us to see all details of the click
+    // }
+
+    // const handelClickAgain = (name, e) => {
+    //     console.log("hello" + name, e.target);
+    // }
+
+    // return (
+    //     <div className="home">
+    //         <h2>Homepage</h2>
+    //         {/* <p>{ name } is { age } years old</p>
+    //         <button onClick={handleClick}>Click Me</button> */}
+    //         {/* <button onClick={(e) => handelClickAgain('Brian', e)}>Click Me Again</button> */}
+    //     </div>
+    // );
+ 
+export default Home;
